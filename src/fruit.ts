508fbbbb956b1dc5b61dbd60ecc8c900f210370a
mattgibson18@outlook.com <mattgibson18@outlook.com>
2024-04-08 15:54:23 +0100

    added the fruit to the grid in random place

---
+++ src/fruit.ts
@@ -0,0 +1,19 @@
+export const addFruit = () => {
+  const gridCells = document.querySelectorAll(".gridCell");
+
+  const availableCells: number[] = [];
+  for (let i = 0; i < gridCells.length; i++) {
+    const cell = gridCells[i] as HTMLElement;
+    if (!cell.classList.contains("snakeSegment")) {
+      availableCells.push(i);
+    }
+  }
+
+  const randomCellIndex =
+    availableCells[Math.floor(Math.random() * availableCells.length)];
+
+  const fruitImage = document.createElement("img");
+  fruitImage.src = "./Banana.png";
+  fruitImage.classList.add("fruit");
+  gridCells[randomCellIndex].appendChild(fruitImage);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import "./styles.scss";
 import { createGrid } from "./grid";
 import { initialiseSnake, handleSpaceKey } from "./initialiseSnake";
+import { addFruit } from "./fruit";
 
 createGrid();
 initialiseSnake(22);
 handleSpaceKey();
+addFruit();
